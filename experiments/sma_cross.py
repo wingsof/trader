15bf5698aeaf6f50353d8d28604f8318ffad8948
Yongwoo Park <nnnlife@gmail.com>
2018-12-06 23:13:42 +0900

    ema example is added to sma_cross

--- experiments/sma_cross.py
+++ experiments/sma_cross.py
@@ -11,30 +11,29 @@ import numpy as np
 
 
 class MovingAverageCross:
-    def __init__(self, data, short_ma, long_ma):
-        self.short_ma = short_ma
-        self.long_ma = long_ma
+    def __init__(self, data, period):
+        self.period = period
         self.df = pd.DataFrame(list(map(lambda x: {'date': time_converter.intdate_to_datetime(x['0']), 'close': x['5']}, data)))
         self.df = self.df.set_index('date')
 
     def generate_signals(self):
         self.df['signal'] = 0.0
-        self.df['short_mavg'] = self.df['close'].rolling(self.short_ma, min_periods=1).mean()
-        self.df['long_mavg'] = self.df['close'].rolling(self.long_ma, min_periods=1).mean()
-        self.df['signal'][self.short_ma:] = np.where(self.df['short_mavg'][self.short_ma:] > self.df['long_mavg'][self.short_ma:], 1.0, 0.0)
+        self.df['mavg'] = self.df['close'].rolling(self.period, min_periods=1).mean()
+        self.df['emavg'] = self.df['close'].ewm(span=self.period, min_periods=1, adjust=False, ignore_na=False).mean()
+        self.df['signal'][self.period:] = np.where(self.df.emavg[self.period:] < self.df['close'][self.period:], 1.0, 0.0)
         self.df['positions'] = self.df['signal'].diff()
         print(self.df)
 
 
 if __name__ == '__main__':
     _, data = stock_chart.get_day_period_data('A005380', datetime(2015, 1, 1), datetime(2018, 11, 30))
-    mac = MovingAverageCross(data, 20, 120)
+    mac = MovingAverageCross(data, 20)
     mac.generate_signals()
     fig = plt.figure()
     fig.patch.set_facecolor('white')
     ax1 = fig.add_subplot(111, ylabel='price')
     mac.df['close'].plot(ax=ax1, color='r', lw=2.)
-    mac.df[['short_mavg', 'long_mavg']].plot(ax=ax1, lw=2.)
+    mac.df[['mavg', 'emavg']].plot(ax=ax1, lw=2.)
 
     ax1.plot(mac.df.ix[mac.df.positions == -1.0].index, 
             mac.df.close[mac.df.positions == -1.0], 'v',
