21bbe57b23a1c04272b2bac2e0609d38062cd4cd
Yongwoo Park <nnnlife@gmail.com>
2019-11-10 18:51:03 +0900

    subscribed source version

--- subscribe_module/stock_daily_insert.py
+++ subscribe_module/stock_daily_insert.py
@@ -2,6 +2,47 @@ import stock_chart
 from datetime import datetime
 import stock_code
 from pymongo import MongoClient
+import win32com.client
+import time_converter
+
+def insert_investor_trend(client, code):
+    obj = win32com.client.Dispatch('CpSysDib.CpSvr7254')
+    obj.SetInputValue(0, code)
+    obj.SetInputValue(1, 6)
+    obj.SetInputValue(4, ord('0'))
+    obj.SetInputValue(5, 0)
+    obj.SetInputValue(6, ord('1'))
+
+    obj.BlockRequest()
+    count = obj.GetHeaderValue(1)
+
+    for i in range(count):
+        d = {}
+        for j in range(19):
+            d[str(j)] = obj.GetDataValue(j, i)
+        if int(d['0'] / 10000) == 0:
+            continue
+        cursor = client.stock[code + '_INV'].find({'0': d['0']})
+        if cursor.count() == 0:
+            client.stock[code + '_INV'].insert_one(d)
+
+
+def insert_short_cover(client, code):    
+    obj = win32com.client.Dispatch('CpSysDib.CpSvr7240')
+    obj.SetInputValue(0, code)
+
+    obj.BlockRequest()
+    count = obj.GetHeaderValue(0)
+
+    for i in range(count):
+        d = {}
+        for j in range(11):
+            d[str(j)] = obj.GetDataValue(j, i)
+        if int(d['0'] / 10000) == 0:
+            continue
+        cursor = client.stock[code + '_COVER'].find({'0': d['0']})
+        if cursor.count() == 0:
+            client.stock[code + '_COVER'].insert_one(d)
 
 
 def daily_insert_data():
@@ -15,3 +56,15 @@ def daily_insert_data():
         l, data = stock_chart.get_day_period_data(code, start_date, end_date)
         client.stock[code + '_D'].drop()
         client.stock[code + '_D'].insert_many(data)
+        insert_investor_trend(client, code)
+        insert_short_cover(client, code)
+
+
+if __name__ == '__main__':
+    """
+    start_date = datetime(2013, 1, 1)
+    end_date = datetime.now()
+    l, data = stock_chart.get_day_period_data('A005490', start_date, end_date)
+    print(data)
+    """
+    daily_insert_data()
