0d1cc38ddb9457f1ecabf7ace7fd74967eb43a57
Yongwoo Park <nnnlife@gmail.com>
2019-12-03 07:38:09 +0900

    extract excel for testing

--- morning/pipeline/strategy/strategy_viewer.py
+++ morning/pipeline/strategy/strategy_viewer.py
@@ -8,18 +8,19 @@ from morning.pipeline.stream.cybos.stock.db.min_tick import MinTick
 from morning.pipeline.converter.cybos.stock.day_tick import StockDayTickConverter
 from morning.pipeline.strategy.stock.minute_suppressed import MinuteSuppressed
 from morning.needle.tick_min_graph_needle import TickMinGraphNeedle
-
+from morning.needle.tick_excel_needle import TickExcelNeedle
 
 from_date = date(2019, 11, 29)
 mt = MinTick(from_date)
 mt.set_target('cybos:A028300')
 tmgn = TickMinGraphNeedle(True)
-
+tan = TickExcelNeedle()
 sdtc = StockDayTickConverter()
 mt.set_output(sdtc)
 ms = MinuteSuppressed()
 
-tmgn.tick_connect(ms)
+#tmgn.tick_connect(ms)
+tan.tick_connect(ms)
 sdtc.set_output(ms)
 
 while mt.received(None) > 0:
