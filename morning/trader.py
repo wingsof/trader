825bdb8818a8b53f1e1518649a5a0e653fb87cd1
Yongwoo Park <yongwoo1.park@lge.com>
2019-12-16 10:31:03 +0900

    use single thread

--- morning/trader.py
+++ morning/trader.py
@@ -1,57 +1,25 @@
-from PyQt5.QtCore import QCoreApplication, QObject, QTimer, pyqtSlot, QEventLoop
-import sys
-import signal
 from morning.logging import logger
+from morning.target_runner import TargetRunner
 
 
 class Trader(QObject):
-    def __init__(self, realtime = True):
+    def __init__(self, code):
         super().__init__()
-
-        self.realtime = realtime
+		self.runner = None
+		self.code = code
         self.account = None
-        self.runner_count = -1
-        self.app = QEventLoop()
-        self.trade_tunnels = []
+        self.pipelines = []
 
-    def ready(self):
-        return True
-
-    def add_tunnel(self, tunnel):
-        self.trade_tunnels.append(tunnel)
+	def add_pipeline(self, p):
+        self.pipelines.append(p)
 
     def handle_trading(self, msg):
+        logger.print('Handle Trading:', msg)
         if self.account:
             self.account.transaction(msg)
-
-    def increment_runner(self):
-        if self.runner_count == -1:
-            self.runner_count = 1
-        else:
-            self.runner_count += 1
-        logger.print('RUNNER(+)', self.runner_count)
-
-    def decrement_runner(self):
-        if self.runner_count > 0:
-            self.runner_count -= 1
-        else:
-            logger.print('Runner finished before started')
-        logger.print('RUNNER(-)', self.runner_count)
-
-        if not self.realtime and self.runner_count is 0:
-            for tt in self.trade_tunnels:
-                tt.wait_threads()
-            QTimer.singleShot(0, self.app.quit)
 
     def set_account(self, account):
         self.account = account
 
-    @pyqtSlot()
-    def _run(self):
-        for tt in self.trade_tunnels:
-            tt.run()
-
-    def run(self):
-        # if not use app.exec(), no way to receive message from QThread signal
-        QTimer.singleShot(1000, self._run)
-        self.app.exec()
+	def run(self):
+		self.runner = TargetRunner(code, self.pipelines, self.handle_trading)
--- morning_main/realtime_kosdaq.py
+++ morning_main/realtime_kosdaq.py
@@ -2,11 +2,11 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from PyQt5.QtCore import QCoreApplication
 from datetime import datetime
+import signal
 
 from morning.trader import Trader
-from morning.trading_tunnel import TradingTunnel
-from morning_main import morning_launcher
 
 from morning.account.cybos_kosdaq_account import CybosKosdaqAccount
 from morning.pipeline.chooser.cybos.db.kosdaq_search_bull_chooser import KosdaqSearchBullChooser
@@ -18,33 +18,37 @@ from morning.pipeline.strategy.stock.realtime_minute_suppressed import RealtimeM
 
 from morning.pipeline.stream.cybos.stock.tick import CybosStockTick
 
-def trading():
-    ksbc = KosdaqSearchBullChooser(datetime.now().date(), False) # not use database to search codes
-    account = CybosKosdaqAccount()
 
-    trader = Trader(True)
-    tunnel = TradingTunnel(trader)
-
-    tunnel.set_chooser(ksbc)
-
+def start_trading(code, account):
+    trader = Trader(code)
     pipeline = {'name': 'kosdaq_bull',
                 'stream': CybosStockTick(),
                 'converter': StockTickConverter(),
                 'filter': [InMarketFilter(), DropDataFilter(1)],
                 'strategy': [RealtimeMinuteSuppressed()],
                 'decision': BoolAndDecision(1, 2)} # trade count = 2
-
-    tunnel.add_pipeline(pipeline)
-    trader.add_tunnel(tunnel)
+	trader.add_pipeline(pipeline)
     trader.set_account(account)
 
-    #for code in ksbc.codes:
-    #    account.set_bidask_monitoring(code)
-    trader.run()
+	trader.run()
+	tunnel.set_code(code)
+
+	return trader
 
 
 if __name__ == '__main__':
-    # TODO block interrupt
-    morning_launcher.morning_launcher(True, trading)
+    app = QCoreApplication(sys.argv)
+    logger.setup_main_log()
+    if allow_interrupt:
+        signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-    
+	traders = []
+    ksbc = KosdaqSearchBullChooser(datetime.now().date(), False) # not use database to search codes
+    account = CybosKosdaqAccount()
+	for code in ksbc.codes:
+		account.set_bidask_monitoring(code)
+		traders.append(start_trading(code, account))
+
+    app.exec()
+    logger.exit_main_log()
+    morning_launcher.morning_launcher(True, trading)
