877b2edfe19cc92cba308f8bfa085ef3bcdab4b6
Yongwoo Park <nnnlife@gmail.com>
2019-12-27 23:30:10 +0900

    remove print data

--- morning_server/handlers/request_pre_handler.py
+++ morning_server/handlers/request_pre_handler.py
@@ -63,7 +63,7 @@ def _get_data_from_db(code, from_date, until_date, db_suffix):
     vacancy_data = list(stock_db[code + '_V'].find({'0': {'$gte':time_converter.datetime_to_intdate(from_date), '$lte': time_converter.datetime_to_intdate(until_date)}}))
     vacancy_days = [time_converter.intdate_to_datetime(d['0']).date() for d in vacancy_data]
     empties = _check_empty_date(days, vacancy_days, working_days)
-    print('DB DATA', db_data, 'EMPTIES', empties)
+    print('DB DATA', len(db_data), 'EMPTIES', empties)
     return db_data, empties
 
 
