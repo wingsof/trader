f1dc719ae2cc30956aed314c2d8ecb60c1dd5d74
Yongwoo Park <nnnlife@gmail.com>
2019-12-28 23:37:30 +0900

    add semaphore

--- morning_server/server_util.py
+++ morning_server/server_util.py
@@ -6,6 +6,8 @@ from utils import time_converter
 from pymongo import MongoClient
 import gevent
 from datetime import timedelta, datetime
+from gevent.lock import Semaphore
+
 
 def stream_write(sock, header, body, manager = None):
     try:
@@ -17,7 +19,8 @@ def stream_write(sock, header, body, manager = None):
 
 
 class _Collector:
-    def __init__(self, sock, capability):
+    def __init__(self, container, sock, capability):
+        self.container = container
         self.sock = sock
         self.capability = capability
         self.subscribe_code = []
@@ -43,7 +46,9 @@ class _Collector:
         return len(self.subscribe_code)
 
     def set_pending(self, is_pending):
+        self.container.semaphore.lock()
         self.request['pending'] = is_pending
+        self.container.semaphore.release()
 
     def append_subscribe_code(self, code):
         self.subscribe_code.append(code)
@@ -57,9 +62,10 @@ class _Collector:
 class CollectorList:
     def __init__(self):
         self.collectors = []
+        self.semaphore = Semaphore()
 
     def add_collector(self, sock, body):
-        self.collectors.append(_Collector(sock, body['capability']))
+        self.collectors.append(_Collector(self, sock, body['capability']))
 
     def handle_disconnect(self, sock):
         print('HANDLE DISCONNECT: CollectorList')
@@ -90,15 +96,18 @@ class CollectorList:
 
     def find_request_collector(self):
         available_collectors = []
+        self.semaphore.acquire()
         for c in self.collectors:
             if c.capability | message.CAPABILITY_REQUEST_RESPONSE and not c.request_pending():
                 available_collectors.append(c)
             
         if len(available_collectors) == 0:
+            self.semaphore.release()
             return None
         
         available_collectors = sorted(available_collectors, key=lambda x: x.latest_request_process_time)
         available_collectors[0].latest_request_process_time = datetime.now()
+        self.semaphore.release()
         return available_collectors[0]
 
     def find_subscribe_collector(self):
