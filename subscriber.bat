b903afa1e82b774b0420c4d748cf9dbf99eb5b0f
Yongwoo Park <nnnlife@gmail.com>
2020-04-09 22:34:17 +0900

    add delay for each collector

--- subscriber.bat
+++ subscriber.bat
@@ -1,20 +1,10 @@
 @echo off
 :loop
 
-set mon=%DATE:~5,2%
-set day=%DATE:~8,2%
-set year=%DATE:~0,4%
-set h=%time:~0,2%
-set m=%time:~3,2%
-set s=%time:~6,2%
-set _time=%h%_%m%_%s%
-set "_time=%_time: =%"
-set filename="%year%%mon%%day%_%_time%.log"
-
 timeout /t 30 /nobreak > NUL
 
 IF EXIST C:\workspace\trader\READY (
-"C:\Users\Administrator\AppData\Local\Programs\Python\Python36-32\python.exe" "C:\workspace\trader\morning_server\collectors\request_client.py" > "C:\workspace\trader\logs\%filename%" 2>&1
+"C:\Users\Administrator\AppData\Local\Programs\Python\Python36-32\python.exe" "C:\workspace\trader\morning_server\collectors\request_client.py"
 )
 
 goto loop
