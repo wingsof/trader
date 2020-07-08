17b846c45e039ce2c21dd0fc4fce7cb884abd52f
nnnlife <nnnlife@gmail.com>
2020-07-08 18:57:17 +0900

    quick fix

--- morning_server/yesterday_top_amount.py
+++ morning_server/yesterday_top_amount.py
@@ -5,8 +5,8 @@ from configs import db
 
 
 def get_yesterday_top_amount(date):
-    db = MongoClient(db.HOME_MONGO_ADDRESS)['trade_alarm']
-    ydata = list(db['yamount'].find({'date': date}))
+    collection = MongoClient(db.HOME_MONGO_ADDRESS)['trade_alarm']
+    ydata = list(collection['yamount'].find({'date': date}))
     if len(ydata) == 1:
-        return ydata['codes']
+        return ydata[0]['codes']
     return []
